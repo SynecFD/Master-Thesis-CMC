2c4e5aef502dbcb27fda2a792e6c3f6a3a06929e
Oliver Ubuntu <3bartels@informatik@uni-hamburg.de>
2021-03-27 16:28:26 +0100

    updated to latest version

--- cmc.py
+++ cmc.py
@@ -7,7 +7,7 @@ from mpi4py import MPI
 from parameters import *
 import tensorflow as tf
 from keras.models import load_model, clone_model, save_model
-from networks import actor, CAE_critic, world, rolledout_world, mpc, compute_initial_plan, create_session, compute_graph_only, sample_random_action
+from networks import actor, CAE_critic, world, rolledout_world, mpc, compute_initial_plan, create_session, compute_graph_only, sample_random_action, get_graph
 import random, numpy as np, env
 from utils import PARSER
 from keras.backend import clear_session
@@ -22,44 +22,66 @@ rank = comm.Get_rank()
 class CMC (object):
     def __init__(self, args):
         self.env = env.make_env(args)
-        self.actor = actor()
-        self.autoencoder_critic, self.encoder, self.critic = CAE_critic()
-        self.critic_t = clone_model(self.critic); self.critic_t.set_weights(self.critic.get_weights())
-        self.actor_t = clone_model(self.actor); self.actor_t.set_weights(self.actor.get_weights())
-        self.world = world()
         self.rolledout_world = rolledout_world()
-        self.load_weights()
+        if(config_args.load_weights):
+            #self.autoencoder_critic, self.autoencoder, self.encoder, self.critic = CAE_critic()
+            #self.actor = actor()
+            #self.world = load_model('world')
+            self.load_complete_models()
+            #self.critic_t = clone_model(self.critic); self.critic_t.set_weights(self.critic.get_weights())
+            #self.actor_t = clone_model(self.actor); self.actor_t.set_weights(self.actor.get_weights())
+        else:
+            self.actor = actor()
+            self.autoencoder_critic, self.autoencoder, self.encoder, self.critic = CAE_critic()
+            self.critic_t = clone_model(self.critic); self.critic_t.set_weights(self.critic.get_weights())
+            self.actor_t = clone_model(self.actor); self.actor_t.set_weights(self.actor.get_weights())
+            #self.world = world()
+            self.world = load_model('world')
+            self.critic_t = clone_model(self.critic); self.critic_t.set_weights(self.critic.get_weights())
+            self.actor_t = clone_model(self.actor); self.actor_t.set_weights(self.actor.get_weights())
         self.memory = deque(maxlen=int((1e+5)/8))
-        self.goals = np.loadtxt("goals")
         
+        self.graph = get_graph()
         self.errors = []
         self.avgs = []
         self.max_avg = 1.
         
     def restart(self): # restart to reset memory and speed up learning
-        #self.env = env.make_env(args)
-        clear_session()
-        create_session()
-        self.actor = actor()
-        self.autoencoder_critic, self.encoder, self.critic = CAE_critic()
-        self.world = world()
-        self.rolledout_world = rolledout_world()
-        self.load_weights()
+        self.sess = create_session()
+        self.graph = get_graph()
+        with self.sess.as_default(): 
+            self.load_complete_models()
+            self.rolledout_world = rolledout_world()
+        #self.actor = actor()
+        #self.autoencoder_critic, self.encoder, self.critic = CAE_critic()
+        #self.world = world()
+        #self.load_weights()
 
     def load_weights(self):
+        self.world.load_weights('./world')
         self.autoencoder_critic.load_weights('./autoencoder_W')
-        self.world.load_weights('./world') 
         self.actor.load_weights('./actor_W') 
         self.critic.load_weights('./critic_W')
+        self.encoder.load_weights('./encoder_W')
+        self.autoencoder.load_weights('./autoencoder_W')
 
         self.critic_t = clone_model(self.critic); self.critic_t.set_weights(self.critic.get_weights())
         self.actor_t = clone_model(self.actor); self.actor_t.set_weights(self.actor.get_weights())
 
     def load_complete_models(self):
-        self.autoencoder_critic = load_model('./autoencoder_W')
-        self.world = load_model('./world') 
-        self.actor = load_model('./actor_W') 
-        self.critic = load_model('./critic_W')
+        #self.autoencoder_critic, self.encoder, self.critic = CAE_critic()
+        self.autoencoder_critic = load_model('autoencoder_critic')
+        self.world = load_model('world') 
+        self.actor = load_model('actor') 
+        self.critic = load_model('critic')
+        self.encoder = load_model('encoder') 
+        self.autoencoder = load_model('autoencoder')
+
+        #self.autoencoder_critic._make_predict_function()
+        self.world._make_predict_function()
+        self.actor._make_predict_function()
+        self.encoder._make_predict_function()
+        #self.critic._make_predict_function()
 
         self.critic_t = clone_model(self.critic); self.critic_t.set_weights(self.critic.get_weights())
         self.actor_t = clone_model(self.actor); self.actor_t.set_weights(self.actor.get_weights())
@@ -108,12 +130,13 @@ class CMC (object):
                 X_a.append(encoded_state); Y_a.append(action)
         
         # update critic and actor (CACLA update)       
-        print(Y_c1.shape)
-        self.autoencoder_critic.fit([X_c1, X_c2], [Y_c1, Y_c2], batch_size=batch_size, epochs=5, verbose=0)
+        tensorboard_callback_autocritic = tf.keras.callbacks.TensorBoard(log_dir="summaries/autoenc_critic", histogram_freq=1)
+        self.autoencoder_critic.fit([X_c1, X_c2], [Y_c1, Y_c2], batch_size=batch_size, epochs=5, verbose=0, callbacks=[tensorboard_callback_autocritic])
         if len(X_a)>0: self.actor.fit(x=np.asarray(X_a), y=np.asarray(Y_a), batch_size=len(X_a), epochs=15, verbose=0)
         
         # update the world model
-        self.world.fit(X_w, [Y_w1, Y_w2], batch_size=batch_size, epochs=10, verbose=0)
+        tensorboard_callback_world = tf.keras.callbacks.TensorBoard(log_dir="summaries/world", histogram_freq=1)
+        self.world.fit(X_w, [Y_w1, Y_w2], batch_size=batch_size, epochs=10, verbose=0, callbacks=[tensorboard_callback_world])
         
         # update target networks
         critic_weights, critic_t_weights = self.critic.get_weights(), self.critic_t.get_weights()
@@ -130,8 +153,42 @@ class CMC (object):
             new_actor_target_weights.append((tnet_update_rate*actor_weights[i])+((1-tnet_update_rate)*actor_t_weights[i]))
         self.actor_t.set_weights(new_actor_target_weights)
 
+    def world_replay(self):
+        batch_size = min(BATCH_SIZE,len(self.memory))
+        minibatch = random.sample(self.memory, batch_size)
+        X_c1, X_c2, X_a, X_w = np.zeros((batch_size,64,64,3)), np.zeros((batch_size,3)), [], np.zeros((batch_size,35))
+        Y_c1, Y_c2, Y_a, Y_w1, Y_w2 = np.zeros((batch_size,64,64,3)), np.zeros((batch_size,)), [], np.zeros((batch_size,32)), np.zeros((batch_size,1))
+        
+        for i in range(batch_size):
+            state, action, ext_r, total_r, next_state, done = minibatch[i] # action is scalar
+            encoded_state = self.encoder.predict(np.array([state]),batch_size=1)[0]
+            target0, target1 = state, total_r
+            if not done:
+                pol_next_t = self.actor_t.predict(np.array([encoded_state]))
+                target1 += gamma * self.critic_t.predict([np.array([next_state]), pol_next_t])[0]
+            X_c1[i], X_c2[i], Y_c1[i], Y_c2[i] = state, action, target0, target1
+            encoded_next_state = self.encoder.predict(np.array([next_state]),batch_size=1)[0]
+            X_w[i], Y_w1[i], Y_w2[i]= np.concatenate([encoded_state,action]), encoded_next_state, ext_r
+            td_err = target1 - self.critic.predict([np.array([state]), np.array([X_c2[i]])], batch_size = 1)[0]
+            if td_err>0.:
+                X_a.append(encoded_state); Y_a.append(action)
+        
+        # update critic and actor (CACLA update)       
+        tensorboard_callback_autocritic = tf.keras.callbacks.TensorBoard(log_dir="summaries/autoenc_critic", histogram_freq=1)
+        self.autoencoder_critic.fit([X_c1, X_c2], [Y_c1, Y_c2], batch_size=batch_size, epochs=5, verbose=0, callbacks=[tensorboard_callback_autocritic])
+
+        # update the world model
+        tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir="summaries/world", histogram_freq=1)
+        self.world.fit(X_w, [Y_w1, Y_w2], batch_size=batch_size, epochs=10, verbose=0, callbacks=[tensorboard_callback])
+
+        critic_weights, critic_t_weights = self.critic.get_weights(), self.critic_t.get_weights()
+        new_critic_target_weights = []
+        w_cnt = len(critic_t_weights)
+        for i in range(w_cnt):
+            new_critic_target_weights.append((tnet_update_rate*critic_weights[i])+((1-tnet_update_rate)*critic_t_weights[i]))
+        self.critic_t.set_weights(new_critic_target_weights)
     
-    def learn (self, nb_episodes = 1000, steps_per_episode = 900, sim = 1, nb_simulation = 1, only_mpc = False):
+    def learn (self, nb_episodes = 1000, steps_per_episode = 400, sim = 1, nb_simulation = 1, only_mpc = False):
         reliable_planner = False
         steps = np.zeros((nb_episodes,))
         rewards = np.zeros((nb_episodes,))        
@@ -144,82 +201,94 @@ class CMC (object):
                 self.env.render()
                 print("Deep ICAC sim: {}/{} episode: {}/{}. Step: {}/{}".format(sim, nb_simulation, i_episode+1, nb_episodes, t+1, steps_per_episode))
                 encoded_obs = self.encoder.predict(np.array([observation]), batch_size = 1)[0]
-                
+
                 if only_mpc: 
                     initial_plan = sample_random_action()
                     action = compute_graph_only(self.world, self.rolledout_world, encoded_obs, initial_plan) # outputs the optimal plan's 1st action
                 elif reliable_planner:
                     initial_plan = compute_initial_plan(encoded_obs, self.actor, self.world)
-                    #print("takes long")
                     action = mpc(self.world, self.rolledout_world, encoded_obs, initial_plan) # outputs the optimal plan's 1st action
                 else:
                     action = self.actor.predict(np.array([encoded_obs]), batch_size = 1)[0]
                     
-                action = np.random.normal(action, std) # exploration noise
-                action[0] = action[0]/2
-                if action[0] < 0.33 and action[0] > -0.33:
-                    action[0] = 0
-                #else:
-                #    action[0] = 0
+                action[0] = np.random.normal(action[0], scale=0.35) # exploration noise
+                action[1] = np.random.normal(action[1], scale=0.35)
+                action[2] = np.random.normal(action[2], scale=0.2)
+                clipped_act = self.action_clip(action, 0)
 
-                action[1] = np.absolute(action[1]) # There is no negative acceleration, breaking is separate
-                if action[1] < 1:
-                    action[1] = 1
-                #elif action[1]>0:
-                #    action[1] = 1
-
-                action[2] = action[2]/2
-                if action[2] < 0:
-                    action[2] = 0
-                elif action[2]>1:
-                    action[2] = 1
-                
-
-                clipped_act = action
                 ready_act = clipped_act
-                #print(ready_act)
-                #ready_act = np.multiply(np.array([clipped_act]),[20])
                 obs_new, r_ext, done, x = self.env.step(ready_act)
-                encoded_next_obs = self.encoder.predict(np.array([obs_new]), batch_size = 1)[0]
-                lp = self.lp(self.world, np.concatenate([encoded_obs, ready_act]), encoded_next_obs, r_ext)
-                r_int = -lp
-                reliable_planner = True if lp>=0.005 else False
-                r_total = r_ext + r_int/(1+(D*(t+1+sum(steps))))
-                total_ext_reward_per_episode += r_ext 
-                
-                self.remember(observation, clipped_act, r_ext, r_total, obs_new, done)   # was exp_action                          
-                
+                if not only_mpc:
+                    encoded_next_obs = self.encoder.predict(np.array([obs_new]), batch_size = 1)[0]
+                    decoded = self.autoencoder.predict([np.expand_dims(obs_new, axis=0)], batch_size = 1)[0]
+                    lp = self.lp(self.world, np.concatenate([encoded_obs, ready_act]), encoded_next_obs, r_ext)
+                    r_int = -lp
+                    reliable_planner = True if lp>=0 else False
+                    r_total = r_ext + r_int/(1+(D*(t+1+sum(steps))))
+                    total_ext_reward_per_episode += r_ext 
+                    self.remember(observation, clipped_act, r_ext, r_total, obs_new, done)   # was exp_action                          
+                else:
+                    total_ext_reward_per_episode += r_ext
                 observation = obs_new
                 if done:
                     break
             
             print("--updating--")
-            self.replay()
-                
+            if only_mpc:
+                self.world_replay
+            else:
+                self.replay()
+            
             steps [i_episode] = t+1
             
             print("--saving networks--")
             try: 
-                self.actor.save_weights("actor_W"); self.autoencoder_critic.save_weights("autoencoder_W"); self.world.save_weights("world")
-                self.critic.save_weights("critic_W")
-                save_model(self.actor,'actor'); save_model(self.autoencoder_critic, 'autoencoder_critic'); save_model(self.world, 'world')
+                if not only_mpc:
+                    self.actor.save_weights("actor_W"); self.autoencoder_critic.save_weights("autoencoder_W")
+                    self.critic.save_weights("critic_W")
+                    
+                    save_model(self.actor,'actor'); save_model(self.autoencoder_critic, 'autoencoder_critic')
+                    save_model(self.critic, 'critic')
+                save_model(self.autoencoder, 'autoencoder')
+                self.autoencoder.save_weights('autoencoder_W')
+                save_model(self.encoder, 'encoder')
+                self.encoder.save_weights('encoder_W')
+                save_model(self.world, 'world')
+                self.world.save_weights("world")
+                #save_model(self.rolledout_world, 'rolledout_world')
             except IOError as e:
                 print("I/O error({0}): {1}".format(e.errno, e.strerror))   
 
             rewards [i_episode] = total_ext_reward_per_episode
             
             print("--saving results--")
-            np.savetxt('rewards',rewards)
-            np.savetxt('steps',steps)
+            np.savetxt('summaries/rewards',rewards)
+            np.savetxt('summaries/steps',steps)
             self.restart()
             #create_session()
         
         self.critic = None; self.critic_t = None; self.actor = None; self.actor_t = None
         
         return rewards, steps 
+    
+    def action_clip(self, action, steer_pref):
+        action[0] = (action[0]) + steer_pref # negative values indicate left steering. shifting the values to [-1,1]
+        if action[0] < 0.1 and action[0] > -0.1:
+            action[0] = 0
+
+        action[1] = (action[1] + 1.1)/2 # There is no negative acceleration, breaking is separate
+        if action[1] > 1:
+            action[1] = 1
+
+        action[2] = action[2] - 0.1
+        if action[2] < 0:
+            action[2] = 0
+        elif action[2]>1:
+            action[2] = 1
+        return action
 
 def sprint(*args):
-    print(args) # if python3, can do print(*args)
+    print(*args) # if python3, can do print(*args)
     sys.stdout.flush()
 
 def main(args):
@@ -241,17 +310,20 @@ def main(args):
 
     #initialize_settings(args.controller_sigma_init, args.controller_sigma_decay)
     
-    only_mpc = True
+    only_mpc = False
 
     sprint("process", rank, "out of total ", comm.Get_size(), "started")
     if(only_mpc):
-        agent = CMC(args)
+        config_args.load_weights = False
+        agent = CMC(config_args)
         rewards, steps = agent.learn(only_mpc=True)
     elif (rank == 0):
-        agent = CMC(args)
+        config_args.load_weights = False
+        agent = CMC(config_args)
         rewards, steps = agent.learn()
     else:
-        agent = CMC(args)
+        config_args.load_weights = True
+        agent = CMC(config_args)
         rewards, steps = agent.learn()
         #slave()
 
--- networks.py
+++ networks.py
@@ -9,16 +9,18 @@ from numpy.random import default_rng
 from keras.callbacks import TensorBoard
 from keras.backend import clear_session
 
-config = tf.compat.v1.ConfigProto(allow_soft_placement=True, log_device_placement=False)
+config = tf.compat.v1.ConfigProto(device_count={'GPU': 1 , 'CPU': 8}, allow_soft_placement=True, log_device_placement=False)
 #new_sess = tf.Session()
 #K.set_session(new_sess)
 gpu_devices = tf.config.experimental.list_physical_devices('GPU')
 for device in gpu_devices:
     tf.config.experimental.set_memory_growth(device, True)
 config.gpu_options.allow_growth = True
-sess = tf.compat.v1.Session(config=config)
+graph = tf.compat.v1.get_default_graph()
+sess = tf.compat.v1.Session(graph=graph, config=config)
 tf.compat.v1.keras.backend.set_session(sess)
 tf.compat.v1.disable_eager_execution()
+init = tf.compat.v1.global_variables_initializer()
 
 laten_code_dim = 32
 action_dim = 3
@@ -27,23 +29,32 @@ input_img_shape = (64, 64, 3)
 
 rng = default_rng()
 
-def create_session() :
+def get_graph():
+    return graph
+
+def create_session():
+    clear_session()
     config = tf.compat.v1.ConfigProto(allow_soft_placement=True, log_device_placement=False)
     gpu_devices = tf.config.experimental.list_physical_devices('GPU')
     for device in gpu_devices:
         tf.config.experimental.set_memory_growth(device, True)
     config.gpu_options.allow_growth = True
-    sess = tf.compat.v1.Session(config=config)
-    tf.compat.v1.keras.backend.set_session(sess)
-    tf.compat.v1.disable_eager_execution()
+    graph = tf.compat.v1.get_default_graph()
+    with graph.as_default():
+        sess = tf.compat.v1.Session(graph=graph, config=config)
+        tf.compat.v1.keras.backend.set_session(sess)
+        tf.compat.v1.disable_eager_execution()
+    init = tf.compat.v1.global_variables_initializer()
+    sess.run(init)
+    return sess
 
 def CAE_critic() :  # Minimized Conv AutoEncoder-Critic Net    
-    input_img = Input(shape=input_img_shape)
+    input_img = Input(shape=input_img_shape, name='input_img')
     input_act = Input(shape=(action_dim,),name='input_act')   
     x = Conv2D(8, (8, 8), strides=4, activation='relu', name='conv1')(input_img) 
     x = Conv2D(16, (4, 4), strides=1,  activation='relu', name='conv2')(x) 
     x = Flatten()(x)
-    encoded = Dense((laten_code_dim), activation='sigmoid', name = 'dense1')(x)
+    encoded = Dense((laten_code_dim), activation='relu', name = 'dense1')(x)
     encoded_with_act = concatenate([encoded, input_act])
      
     x = Dense((1536),activation='relu', name='dense2')(encoded)
@@ -58,11 +69,34 @@ def CAE_critic() :  # Minimized Conv AutoEncoder-Critic Net
     output_val = Dense((1))(h_val)
     critic = Model(inputs = [input_img,input_act], outputs = output_val) 
     encoder = Model(input_img, encoded)
+    autoencoder = Model(input_img, decoded)
     autoencoder_critic = Model([input_img, input_act], [decoded, output_val])
     adam = Adam(lr = 0.001)
     autoencoder_critic.compile(optimizer=adam, loss=['mse','mse'], loss_weights=[0.1,1.]) # loss weighs for AE and Critic losses 
     
-    return autoencoder_critic, encoder, critic 
+    return autoencoder_critic, autoencoder, encoder, critic 
+
+def autoencoder() :  
+    input_img = Input(shape=input_img_shape, name='input_img')
+    x = Conv2D(8, (8, 8), strides=4, activation='relu', name='conv1')(input_img) 
+    x = Conv2D(16, (4, 4), strides=1,  activation='relu', name='conv2')(x) 
+    x = Flatten()(x)
+    encoded = Dense((laten_code_dim), activation='sigmoid', name = 'dense1')(x)
+     
+    x = Dense((1536),activation='relu', name='dense2')(encoded)
+    x = Reshape((12,8,16))(x)
+    x = Conv2D(16, (4, 4), strides=1, padding= 'same', activation='relu', name='dec_conv1')(x)
+    x = UpSampling2D ((2,2))(x) 
+    x = Conv2D(8, (10, 2), strides=1, activation='relu', name='dec_conv2')(x)
+    x = UpSampling2D ((5,5))(x) 
+    decoded = Conv2D(3, (12, 12), strides=1, activation='sigmoid', name='dec_conv3')(x) 
+    
+    encoder = Model(input_img, encoded)
+    autoencoder = Model(input_img, decoded)
+    adam = Adam(lr = 0.001)
+    autoencoder.compile(optimizer=adam, loss='mse') 
+    
+    return autoencoder, encoder
 
 def actor():
     input_ = Input(shape=(32,))
@@ -111,18 +145,15 @@ def rolledout_world(): # rolling out the world 3 timesteps into the future
     return model        
 
 def mpc(world, rolledout_world, state, initial_plan): # Model Predictive Control
-    lr, epochs = 0.02, 5 # learning rate and no. of training epochs (optimization iterations)
+    lr, epochs = 0.01, 5 # learning rate and no. of training epochs (optimization iterations)
     _transfer_weights(world, rolledout_world)
     outputTensor1, outputTensor2, outputTensor3 = rolledout_world.output[0], rolledout_world.output[1], rolledout_world.output[2]
     loss = 0.5*(K.square(1.-outputTensor1)+K.square(1.-outputTensor2)+K.square(1.-outputTensor3)) # loss_plan
     gradients = K.gradients(loss, rolledout_world.input)
     func = K.function([rolledout_world.input[0],rolledout_world.input[1],rolledout_world.input[2]], gradients)
     for _ in range(epochs):
-        
         input0 = np.array([np.concatenate((state, initial_plan[0]))])
-        #print("start")
         grad_val = func([input0, np.array([initial_plan[1]]), np.array([initial_plan[2]])])
-        #print("end")
         g1, g2, g3 = grad_val[0][0][laten_code_dim:], grad_val[1][0], grad_val[2][0] # gradient of loss_plan w.r.t. each individual action in initial_plan
         updated_plan = np.array([initial_plan[0,0]-lr*g1, initial_plan[1,0]-lr*g2, initial_plan[2,0]-lr*g3])
         initial_plan = updated_plan.reshape((-1,3))
@@ -156,7 +187,7 @@ def compute_graph_only(world, rolledout_world, state, initial_plan):
     return initial_plan[0] # returns the optimal plan's 1st action
 
 def sample_random_action():
-    initial_action = rng.standard_normal(size=(3,3))
+    initial_action = (2 * rng.random(size=(3,3))) -1
     #print(initial_action)
     return initial_action
 
